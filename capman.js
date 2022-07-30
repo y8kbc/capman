02fb61ff4ed454fcb12b77cb0a18e8d3e47a54c7
zwm <program.py@gmail.com>
2022-07-30 12:30:43 -0700

    Add list of sentences

--- capman.js
+++ capman.js
@@ -5,3 +5,13 @@ codes.forEach(c => {
         console.log(c.innerText);
     });
 })
+
+const answers = [
+    "There were three shepherd rocks congregating in a cubed room",
+    "The murder hornet was disappointed by the preconceived ideas people had of him",
+    "There was no telling what thoughts would come from the machine",
+    "The tart lemonade quenched her thirst, but not her longing",
+    "The Japanese yen for commerce is still well-known",
+    "The sight of his goatee made me want to run and hide under my sister-in-law's bed",
+    "The secret ingredient to his wonderful life was crime",
+]
