231d5f905b3fdda1b137b6f826cb9d09d8248897
andy w <109485721+y8kbc@users.noreply.github.com>
2022-09-04 19:11:44 -0700

    Add listener on letters

--- capman.js
+++ capman.js
@@ -1,12 +1,52 @@
+// TO-DO:
+// - How to keep track and know if all letters have been guessed correctly?
+// - Should I convert null in object to space??
+
+let chances = 6;
+let guesses = [];
+let sentence = ["There were three shepherd rocks congregating in a cubed room"];
+sentence = sentence[0].split('');
+let matches = [];
+let correct = [];
+let temp_word_sentence = [];
+let minecraft = [];
+
+// split setence to individual characters
+// sentence[0].split('').forEach(letter => {
+//     console.log(letter);
+// })
+// console.log(guesses);
+
+// for (let i = 0; i < sentence.length; i++) {
+//     console.log(typeof(sentence[i].toLowerCase()));
+// }
+
 let code = document.querySelectorAll(".code");
 let codes = [...code];
 codes.forEach(c => {
     c.addEventListener('click', function () {
-        console.log(c.innerText);
+        for (let i = 0; i < sentence.length; i++) {
+            if (c.innerText.toLowerCase() == sentence[i].toLowerCase()) {
+                matches[i] = {
+                    index: i,
+                    letter: sentence[i],
+                    match: true
+                };
+                console.log('correct!');
+            }
+        }
+
+        if (!guesses.includes(c.innerText)) {
+            guesses.push(c.innerText);
+            c.innerText = "\u0336" + c.innerText + "\u0336";
+        }
+
+        console.log('guesses: ' + guesses);
+        console.log('matches: ' + JSON.stringify(matches));
     });
 })
 
-const answers = [
+const sentences = [
     "There were three shepherd rocks congregating in a cubed room",
     "The murder hornet was disappointed by the preconceived ideas people had of him",
     "There was no telling what thoughts would come from the machine",
@@ -16,18 +56,20 @@ const answers = [
     "The secret ingredient to his wonderful life was crime",
 ]
 
+
+
 // Find answer's length without spaces:
 // console.log(answers[4].length - answers[4].split(" ").length + 1);
 
-let _code = new Array();
-for (let v of answers) {
-    let _answers = v.split(' ');
-//    console.log(_answers);
-    for (let word of _answers) {
-//        console.log(char);
-        for (let char of word) {
-            _code.push(char);
-        }
-    }
-    console.log(_code);
-}
+// let _code = new Array();
+// for (let v of answers) {
+//     let _answers = v.split(' ');
+// //    console.log(_answers);
+//     for (let word of _answers) {
+// //        console.log(char);
+//         for (let char of word) {
+//             _code.push(char);
+//         }
+//     }
+//     console.log(_code);
+// }
